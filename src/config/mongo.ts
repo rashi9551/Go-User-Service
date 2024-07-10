8bbb50dced646f7659d0fb4cf97a2b5da79b3e27
rashi9551 <rashidroz1361@gmail.com>
2024-07-11 00:47:32 +0530

    compose done perfectly

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -6,14 +6,13 @@ dotenv.config()
 
 const connectDB=async():Promise<void>=>{
     try {
-        const  url=process.env.MONGO_URL
-        if(!url)
+        const MONGO_URLURL=process.env.NODE_ENV==='dev'?process.env.MONGO_URL_DEV:process.env.MONGO_URL_PRO
+        if(!MONGO_URLURL)
             {
                 throw new Error("MONGO_URL is not defined in environment variables.")
             }
-            await mongoose.connect(url)
+            await mongoose.connect(`${MONGO_URLURL}/Go-user`)
             console.log("database Connected");
-            
     } catch (error) {
         console.error('Error connecting to MongoDB:', error) 
     }
